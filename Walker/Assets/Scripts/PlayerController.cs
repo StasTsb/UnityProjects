84fc8428200db0d98e95a956e802f7eb5ec1291d
StasTsb <stastsb@gmail.com>
2021-11-16 22:39:11 +0300

    444

--- Walker/Assets/Scripts/PlayerController.cs
+++ Walker/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,91 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController controller;
+    private Animator anim;
+    private Vector3 dir;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    [SerializeField] private GameObject losePanel;
+
+    
+
+    private int lineToMove = 1;
+    public float lineDistance = 4;
+
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (SwipeController.swipeRight)
+        {
+            if (lineToMove < 2)
+                lineToMove++;
+        }
+
+        if (SwipeController.swipeLeft)
+        {
+            if (lineToMove > 0)
+                lineToMove--;
+        }
+
+        if (SwipeController.swipeUp)
+        {
+            if (controller.isGrounded)
+                Jump();
+        }
+
+        if (controller.isGrounded)
+            anim.SetTrigger("isRunning");
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (lineToMove == 0)
+            targetPosition += Vector3.left * lineDistance;
+        else if (lineToMove == 2)
+            targetPosition += Vector3.right * lineDistance;
+
+        if (transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+            controller.Move(moveDir);
+        else
+            controller.Move(diff);
+
+        speed += 0.1f * Time.deltaTime;
+
+        if (speed <= 1)  // ЗАКРЫТИЕ ИГРЫ
+        {
+            
+            Application.Quit();    
+        }
+    }
+
+    private void Jump()
+    {
+        dir.y = jumpForce;
+        anim.SetTrigger("isJumping");
+    }
+
+    void FixedUpdate()
+    {
+        dir.z = speed;
+        dir.y += gravity * Time.fixedDeltaTime;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "obstacle")
+        {
+            losePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
