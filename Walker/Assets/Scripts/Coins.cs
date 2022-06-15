db63a2da98748ae6aab630cf547178eec48b4b91
StasTsb <stastsb@gmail.com>
2022-06-15 23:02:41 +0300

    333

--- Walker/Assets/Scripts/Coins.cs
+++ Walker/Assets/Scripts/Coins.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coins : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void Update() 
     {
         transform.Rotate(0, 90 * Time.deltaTime, 0);
-    }
+    }  
+    
 }
